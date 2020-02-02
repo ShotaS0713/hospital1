5ed2cee076411a16195e9fdb13dd58d191113d1f
shimish000 <57582823+shimish000@users.noreply.github.com>
2020-02-02 20:03:44 +0900

    Initial commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,150 @@
+
+
+
+
+
+
+<!doctype html>
+  <html lang="en">
+    <head>
+      <!-- Required meta tags -->
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+      <!-- Bootstrap CSS -->
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+      <link rel="stylesheet" href="{{ asset('css/home.css') }}">
+      <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
+      <title>myhomepage2</title>
+    </head>
+    <body class="bbody">
+      <div class="hfeed_hite">
+        <div class="site_header">
+          <div class="site_top">
+            <div class="site_top_table">
+              <div class="site_blanding">
+                <h1 class="site_title">
+                  <a href="" class="sitename">My Style</a>
+                </h1>
+                <div class="site_description">自分自身を知る</div>
+              </div>
+            
+              <nav class="site_nav">
+                <button class="screenmenu"> 
+                  <span class="screen-reader-text">メニュー</span>
+                  <span class="drawer-hambugar-icon"></span>
+                </button>
+                <div class="drawer-nav">
+                  <div class="drawer-content">
+                    <div class="draw-content__inner">
+                      <ul class="menu-items">
+                        <li class="menu-item">
+                          <a href="" class="homebtn">ホーム <i class="fas fa-chevron-down"></i></a>
+                         
+                          <ul class="sub-menu">
+                            <li>
+                              <a href="">カスタマイズ</a>
+                            </li>
+                          </ul>
+                        </li>
+                        <li class="menu-item">
+                          <a href="">ページ <i class="fas fa-chevron-down"></i></a>
+                          <ul class="sub-menu">
+                            <li>
+                              <a href=""></a>
+                            </li>
+                          </ul>
+                        </li>
+                        <li class="menu-item">
+                          <a href="">お問い合わせ</a>
+                          <ul class="sub-menu">
+                            <li>
+                              <a href=""></a>
+                            </li>
+                          </ul>
+                        </li>
+                      </ul>
+                    </div>
+                    <form action="" id="sss" method="post">
+                      <label for="">
+                        <span></span>
+                        <input type="search" class="search-field" placeholder="検索..."　value name="s" >
+                      </label>
+                      <button type="submit" class="search-submit" value="検索">
+                      <i class="fas fa-search"></i>
+                      </button>
+                    </form>
+                  </div>
+                </div>
+              </nav>
+            </div>
+          </div>
+          <div class="header-image">
+            <img src="/storage/glasses.jpg" alt="" class="glasses">
+          </div>
+          <div class="home-text">
+          My Styleは文章を書く人のためのミニマルなWordPressテーマです。装飾を極限まで省いたデザインは、あなたの文章を主役にします。
+          </div>
+          <div class="site-content">
+
+          </div>
+          <footer class="site-footer">
+            <div class="footer-profile-area">
+              <div class="footer-profile-table">
+                <div class="footer-profile-side">
+                  <div class="profile-content">
+                    <div class="selfintro">
+                      <div class="profimg">
+                        <img src="/storage/glasses.jpg" alt="" class="profpic">
+                      </div>
+                      <div class="prof-meta">
+                        <div class="profname">
+                          <strong>Shota Shimizu</strong>
+                        </div>
+                        <div class="profsns">
+                          <i class="fab fa-twitter"></i>
+                          <i class="fab fa-instagram"></i>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="proftext">
+                      Birth:  1995.06.08 <br>
+                      Birthplace:  Osaka <br>
+                      Specialty:  Ball game(sports) <br>
+
+                    </div>
+                  </div>
+                </div>
+                <div class="footer-profile-main">
+                  <div class="pf profile-1"></div>
+                  <div class="pf profile-2"></div>
+                  <div class="pf profile-3"></div>
+                </div>
+              </div>
+            </div>
+            <div class="site-bottom">
+              <div class="site-bottom__table">
+                <nav class="footer-social-link">
+                  <ul class="social-icon">
+                    <li class="fabli"><i class="fab fa-twitter"></i></li>
+                    <li class="fabli"><i class="fab fa-instagram"></i></li>
+                  </ul>
+                </nav>
+                <div class="site-info">
+                  <div class="site-credit">
+                    	Powered by WordPress | Theme: Write by Themegraphy
+                  </div>
+                </div>
+              </div>
+            </div>
+          </footer>
+        </div>
+      </div>
+
+      <!-- Optional JavaScript -->
+      <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+      <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+      <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    </body>
+  </html>
