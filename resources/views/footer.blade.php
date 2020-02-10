40004c4b993ceb7d588e4710fc9acd0e6252cffd
shimish000 <57582823+shimish000@users.noreply.github.com>
2020-02-10 20:35:01 +0900

    contact.blade.php

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,53 @@
+<footer class="site-footer">
+            <div class="footer-profile-area">
+              <div class="footer-profile-table">
+                <div class="footer-profile-side">
+                  <div class="profile-content">
+                    <div class="selfintro">
+                      <div class="profimg">
+                        <img src="/storage/glasses.jpg" alt="" class="profpic">
+                      </div>
+                      <div class="prof-meta">
+                        <div class="profname">
+                          <strong>Shota Shimizu</strong>
+                        </div>
+                        <div class="profsns">
+                          <i class="fab fa-twitter"></i>
+                          <i class="fab fa-instagram"></i>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="proftext">
+                      Birth:  1995.06.08 <br>
+                      Birthplace:  Osaka <br>
+                      Specialty:  Ball game(sports) <br>
+
+                    </div>
+                  </div>
+                </div>
+                <div class="footer-profile-main">
+                  <div class="pf profile-1"></div>
+                  <div class="pf profile-2"></div>
+                  <div class="pf profile-3"></div>
+                </div>
+              </div>
+            </div>
+            <div class="site-bottom">
+              <div class="site-bottom__table">
+                <nav class="footer-social-link">
+                  <ul class="social-icon">
+                    <li class="fabli"><i class="fab fa-twitter"></i></li>
+                    <li class="fabli"><i class="fab fa-instagram"></i></li>
+                  </ul>
+                </nav>
+                <div class="site-info">
+                  <div class="site-credit">
+                      Powered by WordPress | Theme: Write by Themegraphy
+                      
+                  </div>
+                </div>
+              </div>
+            </div>
+          </footer>
+        </div>
+      </div>
