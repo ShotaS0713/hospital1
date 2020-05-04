bf0d8f5c515874beeb84e4309e54ac246f0414fc
shimish000 <57582823+shimish000@users.noreply.github.com>
2020-05-04 18:31:29 +0900

    Update header.blade.php

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -7,9 +7,9 @@
 
       <!-- Bootstrap CSS -->
       <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-      <link rel="stylesheet" href="{{ asset('css/home.css') }}">
-      <link rel="stylesheet" href="{{ asset('css/about.css') }}">
-      <link rel="stylesheet" href="{{ asset('css/apps.css') }}">
+      <link rel="stylesheet" href="{{ mix('css/home.css') }}">
+      <link rel="stylesheet" href="{{ mix('css/about.css') }}">
+      <link rel="stylesheet" href="{{ mix('css/apps.css') }}">
       <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
       <title>aboutme</title>
     </head>  
